61a6165a39222be86c8e66dd32e31a3831d6b079
Im Sung Eun <se2id@naver.com>
2023-03-13 15:53:03 +0900

    feat:카테고리 구현

--- client/src/components/Carousel.tsx
+++ client/src/components/Carousel.tsx
@@ -4,6 +4,10 @@ import "slick-carousel/slick/slick-theme.css";
 import styled from "styled-components";
 
 const Body = styled.div`
+	.slick-dots {
+		position: relative;
+		bottom: 0;
+	}
 	.slick-prev:before,
 	.slick-next:before {
 		font-size: 50px;
@@ -14,7 +18,7 @@ const Body = styled.div`
 	}
 `;
 const Style1 = styled.div`
-	padding-left: 10%;
+	justify-content: center;
 	background-color: ${(props) => props.color};
 	display: flex;
 	align-items: flex-end;
@@ -69,28 +73,19 @@ export default function Carousel() {
 			<Slider {...settings}>
 				<div>
 					<Style1 color="green">
-						<img
-							src="https://img.freepik.com/premium-vector/set-of-volunteer-people-illustration_95397-113.jpg?w=2000"
-							alt="1"
-						></img>
+						<img src="/images/home/main-img-1.png" alt="1"></img>
 						<span>작은 마음이 모여서 조금 더 나은 내일을 만들어요.</span>
 					</Style1>
 				</div>
 				<div>
 					<Style1 color="#898D55">
-						<img
-							src="https://www.freebie-ac.jp/sozai/2020_168_volunteering_collection/img/main.jpg"
-							alt="2"
-						></img>
+						<img src="/images/home/main-img-2.png" alt="2"></img>
 						<span>작은 마음이 모여서 조금 더 나은 내일을 만들어요.</span>
 					</Style1>
 				</div>
 				<div>
 					<Style1 color="#767EC6">
-						<img
-							src="https://mblogthumb-phinf.pstatic.net/MjAxODA0MDlfMTUy/MDAxNTIzMjQwNjYxMDcw.NUntdy9kjN5iiUuneXi71eg74XEAtAJ40QOGb1LhOewg.UyZCO_3r6zQUwbDJ9n5FXuX-yalD0bMAHFiy9v8NN1Mg.PNG.lifesaverclinic/%EC%A7%B1%EA%B5%AC-%EC%BB%B4%ED%93%A8%ED%84%B0-%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B46.png?type=w800"
-							alt="3"
-						></img>
+						<img src="/images/home/main-img-3.png" alt="3"></img>
 						<span>작은 마음이 모여서 조금 더 나은 내일을 만들어요.</span>
 					</Style1>
 				</div>
--- client/src/pages/volunteer/Volunteer.tsx
+++ client/src/pages/volunteer/Volunteer.tsx
@@ -1,21 +1,27 @@
 import Card from "../../components/Card";
 import { FaEnvira } from "react-icons/fa";
 import SearchBar from "../../components/volunteer/SearchBar";
+import Carousel from "../../components/Carousel";
+import Kategorie from "../../components/volunteer/Kategorie";
 
 export default function Volunteer() {
 	return (
-		<div style={{ margin: "50px" }}>
-			<SearchBar placeholder="검색어를 입력해 주세요." width={250} height={45} radius={10} />
-			<Card
-				width={380}
-				height={350}
-				src="/images/home/main-img-1.png"
-				title="폐가구를 정리해요"
-				date="일시 : 2023년 3월 10일 14~16시(2h)"
-				place="장소 : 서울시 광진구"
-				person="5 / 8"
-				category={<FaEnvira size={24} />}
-			/>
-		</div>
+		<>
+			<Carousel />
+			<Kategorie />
+			<div style={{ margin: "50px" }}>
+				<SearchBar placeholder="검색어를 입력해 주세요." width={250} height={45} radius={10} />
+				<Card
+					width={380}
+					height={350}
+					src="/images/home/main-img-1.png"
+					title="폐가구를 정리해요"
+					date="일시 : 2023년 3월 10일 14~16시(2h)"
+					place="장소 : 서울시 광진구"
+					person="5 / 8"
+					category={<FaEnvira size={24} />}
+				/>
+			</div>
+		</>
 	);
 }
