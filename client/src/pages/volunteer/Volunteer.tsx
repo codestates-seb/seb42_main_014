5249f3f9f5b3a8f7baccf8e162fcc5e8cc0c9cc7
jellyjw <104891203+jellyjw@users.noreply.github.com>
2023-03-14 11:00:59 +0900

    feat: 봉사활동 페이지 수정

---
+++ client/src/pages/volunteer/Volunteer.tsx
@@ -0,0 +1,25 @@
+import { FaEnvira } from "react-icons/fa";
+import Carousel from "../../components/Carousel";
+import Card from "../../components/volunteer/Card";
+import Category from "../../components/volunteer/Category";
+import SearchBar from "../../components/volunteer/SearchBar";
+
+export default function Volunteer() {
+	return (
+		<>
+			<SearchBar placeholder="검색어를 입력해 주세요." width={250} height={45} radius={10} />
+			<Carousel />
+			<Category />
+			<Card
+				width={380}
+				height={350}
+				src="/images/home/main-img-1.png"
+				title="폐가구를 정리해요"
+				date="일시 : 2023년 3월 10일 14~16시(2h)"
+				place="장소 : 서울시 광진구"
+				person="5 / 8"
+				category={<FaEnvira size={24} />}
+			/>
+		</>
+	);
+}
