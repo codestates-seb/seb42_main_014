8da387a07aa3819fb6e36390b3c8f181c6f0e38a
uyun Lee <2uyyn2@gmail.com>
2023-03-17 22:24:00 +0900

    feat: 비밀번호 확인 기능 추가

--- server/volunteer/src/main/java/com/main/volunteer/domain/member/dto/LoginDto.java
+++ server/volunteer/src/main/java/com/main/volunteer/domain/member/dto/LoginDto.java
@@ -1,8 +1,10 @@
 package com.main.volunteer.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginDto {
     private String email;
     private String password;
