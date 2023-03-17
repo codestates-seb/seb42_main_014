d07420a0eabaae8ba2e414be1bec85d10eb6d479
개발메모 <88031716+kms310272@users.noreply.github.com>
2023-03-17 09:43:42 +0900

    Feat/be/group (#88)
    * feat:group crud 완료, comment 생성
    
    * feat:groupController response
    
    * remove: mapstruct ver 2개중 하나 삭제
    
    * feat: comment package 생성, CRUD 기능 구현
    
    feat: comment package 생성, CRUD 기능 구현
    
    * feat: 참조순환 방지 어노테이션 추가
    
    feat: 참조순환 방지 어노테이션 추가
    
    * member 경로 변경 적용 안된 부분 다시 적용
    
    member 경로 변경 적용 안된 부분 다시 적용
    
    * 그룹 가입하기, 가입한 그룹 조회하기 기능 memberGroup 추가
    
    feat: 그룹 가입하기, 가입한 그룹 조회하기 기능 memberGroup 추가
    
    * feat: 사용자 인증 추가
    
    feat: 사용자 인증 추가
    
    * 에러뜬 부분때문에 수정후 커밋
    
    에러뜬 부분때문에 수정후 커밋
    
    * groupZangId 로 변경
    
    groupZangId 로 변경
    
    * 그룹장 체크 하는 메서드 수정
    
    그룹장 체크 하는 메서드 수정
    
    * fix: groupZangId 변경
    
    * fix: @JsonIgnore 삭제
    
    fix: @JsonIgnore 삭제
    
    ---------
    
    Co-authored-by: [minso kim] <“[bella.minsokim@gmail.com]”>

--- server/volunteer/src/main/java/com/main/volunteer/domain/group/mapper/GroupMapper.java
+++ server/volunteer/src/main/java/com/main/volunteer/domain/group/mapper/GroupMapper.java
@@ -11,20 +11,20 @@ public interface GroupMapper {
 
     Group groupPostDtoToGroup(GroupDto.Post postDto);
     Group groupPatchDtoToGroup(GroupDto.Patch patchDto);
-
+    @Mapping(source = "member.memberId", target = "groupZangId")
    default GroupDto.Response groupToGroupResponseDto(Group group) {
 
        long groupId = group.getGroupId();
        String groupName = group.getGroupName();
        String groupImage = group.getGroupImage();
-       long groupLeaderId = group.getGroupZangId();
+       long groupZangId = group.getGroupZangId();
        int applyLimit = group.getApplyLimit();
        String place = group.getPlace();
        String content = group.getContent();
        long tagId = group.getTag().getTagId();
        String tagName = group.getTag().getTagName();
 
-       return new GroupDto.Response(groupId, groupName, groupImage, groupLeaderId, applyLimit, place, content,tagId,tagName);
+       return new GroupDto.Response(groupId, groupName, groupImage, groupZangId, applyLimit, place, content,tagId,tagName);
    }
 
     List<GroupDto.Response> GroupsToGroupResponseDtos(List<Group> groups);
