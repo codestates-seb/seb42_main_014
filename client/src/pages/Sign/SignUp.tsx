201d9d42c441bc8a3d05beaebad979e1767aed3f
jellyjw <wldnzzz@naver.com>
2023-03-18 00:45:10 +0900

    feat: 회원가입페이지 react-hook-form 적용

--- client/src/pages/Sign/SignUp.tsx
+++ client/src/pages/Sign/SignUp.tsx
@@ -23,8 +23,6 @@ const StyledContainer = styled.div`
 const LoginForm = styled.div`
 	display: flex;
 	flex-direction: column;
-	/* justify-content: center; */
-	/* align-items: center; */
 	width: fit-content;
 `;
 const LoginHeader = styled.h1`
@@ -55,7 +53,6 @@ const Btn = styled.button`
 	margin-top: 20px;
 	padding: 0.5rem;
 `;
-
 const Radio = styled.div`
 	label {
 		font-size: 18px;
@@ -156,89 +153,163 @@ const SignUpHeader = styled.div`
 
 export default function SignUp() {
 	const [isOpen, setisOpen] = useState(false);
-
 	const [isUser, setisUser] = useState<boolean>(true);
+	const [confirmPassword, setConfirmPassword] = useState("");
+
+	const {
+		register,
+		handleSubmit,
+		getValues,
+		reset,
+		formState: { errors },
+	} = useForm<ILoginUser>({ mode: "onSubmit" });
 
 	const toggle = () => {
 		setisOpen(!isOpen);
 	};
 	const User = () => {
 		setisUser(false);
+		reset();
 	};
 	const Company = () => {
 		setisUser(true);
+		reset();
 	};
 
 	interface ILoginUser {
 		email: string;
 		password: string;
+		memberName?: string;
+		companyName?: string;
+		address?: string;
+		businessNumber?: number;
 	}
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm<ILoginUser>({ mode: "onSubmit" });
+
+	const onSubmit = (data: any) => {
+		console.log(data);
+	};
 
 	return (
 		<Body>
-			<SignUpHeader>
-				<LoginHeader>SignUp</LoginHeader>
-				<Radio>
-					<label>
-						<input onClick={User} type="radio" name="contact" value="person" />
-						<span>개인</span>
-					</label>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<SignUpHeader>
+					<LoginHeader>SignUp</LoginHeader>
+					<Radio>
+						<label>
+							<input onClick={User} type="radio" name="contact" value="person" />
+							<span>개인</span>
+						</label>
 
-					<label>
-						<input onClick={Company} type="radio" name="contact" value="corporate" />
-						<span>봉사단체</span>
-					</label>
-				</Radio>
-			</SignUpHeader>
-			<StyledContainer>
-				<LoginForm>
-					{isUser ? (
+						<label>
+							<input onClick={Company} type="radio" name="contact" value="corporate" />
+							<span>봉사단체</span>
+						</label>
+					</Radio>
+				</SignUpHeader>
+				<StyledContainer>
+					<LoginForm>
 						<>
-							<Login placeholder="이메일"></Login>
-							<Login placeholder="패스워드"></Login>
-							<Login placeholder="패스워드 확인"></Login>
-							<Login placeholder="상호명"></Login>
-							<Login placeholder="사업장 주소"></Login>
-							<Login placeholder="사업자 등록 번호"></Login>
+							<Login
+								{...register("email", {
+									required: true,
+									pattern: {
+										value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+										message: "이메일 형식에 맞게 입력해주세요.",
+									},
+								})}
+								placeholder="이메일(필수)"
+							></Login>
+							{errors.email && <div>{errors.email?.message}</div>}
+							<Login
+								{...register("password", {
+									required: true,
+									pattern: {
+										value: /^(?=.*\d)(?=.*[a-z])(?=.*[!@#$%^&*])(?=.*[a-z]).{8,50}$/,
+										message: "8자리 이상 소문자+숫자+특수문자 조합이에요.",
+									},
+								})}
+								placeholder="패스워드(필수)"
+								type="password"
+								minLength={8}
+								maxLength={50}
+							></Login>
+							{errors.password && <div>{errors.password?.message}</div>}
+							<Login
+								type="password"
+								placeholder="패스워드 확인(필수)"
+								onChange={(e) => setConfirmPassword(e.target.value)}
+							></Login>
+							{getValues("password") !== confirmPassword && confirmPassword ? (
+								<div>비밀번호가 일치하지 않아요!</div>
+							) : null}
 						</>
-					) : (
-						<>
-							<Login placeholder="이메일"></Login>
-							<Login placeholder="패스워드"></Login>
-							<Login placeholder="패스워드 확인"></Login>
-							<Login placeholder="닉네임"></Login>
-						</>
-					)}
+						{isUser ? (
+							<>
+								<Login
+									{...register("companyName", {
+										required: true,
+									})}
+									placeholder="상호명(필수)"
+								></Login>
+								<Login
+									{...register("address", { required: true })}
+									placeholder="사업장 주소(필수)"
+								></Login>
+								<Login
+									{...register("businessNumber", {
+										required: true,
+										pattern: {
+											value: /^[0-9]+$/,
+											message: "숫자만 입력할 수 있어요.",
+										},
+									})}
+									placeholder="사업자 등록 번호(필수)"
+									maxLength={10}
+								></Login>
+								{errors.businessNumber && <div>{errors.businessNumber?.message}</div>}
+							</>
+						) : (
+							<>
+								<Login
+									{...register("memberName", {
+										required: true,
+										pattern: {
+											value: /^[a-z]{6,}$/,
+											message: "8자리 이하 영어 소문자로 입력해주세요.",
+										},
+									})}
+									placeholder="닉네임(8자리 이하 영소문자)"
+									maxLength={8}
+								></Login>
+								{errors.memberName && <div>{errors.memberName?.message}</div>}
+							</>
+						)}
 
-					<Flex>
-						<span>약관동의</span>
-						<CheckBox>
-							<input type="Checkbox" />
-						</CheckBox>
-						<span>정보 수집 및 이용 동의 (필수)</span>
-						<span onClick={toggle}>내용보기</span>
-					</Flex>
+						<Flex>
+							<span>약관동의</span>
+							<CheckBox>
+								<input type="Checkbox" />
+							</CheckBox>
+							<span>정보 수집 및 이용 동의 (필수)</span>
+							<span onClick={toggle}>내용보기</span>
+						</Flex>
 
-					<Btn>회원가입</Btn>
-				</LoginForm>
-			</StyledContainer>
-			<Modal isOpen={isOpen} toggle={toggle}>
-				<FlexClo>
-					<div>
-						<h2>개인정보 이용에 대한 안내</h2>
-						<p>[개인정보 보호법] 제21조에 따라 처리합니다.</p>{" "}
-						<p>본인은 개인정보 수집 및 이용에 대하여 동의합니다.</p>{" "}
-						<p>본인은 고유식별정보 수집에 동의합니다.</p>※ 귀하는 개인정보 및 고유식별정보 수집에
-						대한 동의를 거부할 권리가 있습니다.
-					</div>
-					<Btn onClick={toggle}>확인</Btn>
-				</FlexClo>
-			</Modal>
+						<Btn>회원가입</Btn>
+					</LoginForm>
+				</StyledContainer>
+				<Modal isOpen={isOpen} toggle={toggle}>
+					<FlexClo>
+						<div>
+							<h2>개인정보 이용에 대한 안내</h2>
+							<p>[개인정보 보호법] 제21조에 따라 처리합니다.</p>{" "}
+							<p>본인은 개인정보 수집 및 이용에 대하여 동의합니다.</p>{" "}
+							<p>본인은 고유식별정보 수집에 동의합니다.</p>※ 귀하는 개인정보 및 고유식별정보 수집에
+							대한 동의를 거부할 권리가 있습니다.
+						</div>
+						<Btn onClick={toggle}>확인</Btn>
+					</FlexClo>
+				</Modal>
+			</form>
 		</Body>
 	);
 }
