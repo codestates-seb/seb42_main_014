cddb870a0b3f5f7f340d5e744ad48028eaa8c9b9
[minso kim] <[bella.minsokim@gmail.com]>
2023-03-21 23:29:04 +0900

    fix: comment 인증 관련 추가

--- server/volunteer/src/main/java/com/main/volunteer/domain/comment/repository/CommentRepository.java
+++ server/volunteer/src/main/java/com/main/volunteer/domain/comment/repository/CommentRepository.java
@@ -1,11 +1,16 @@
 package com.main.volunteer.domain.comment.repository;
 
+import com.main.volunteer.auth.CustomUserDetails;
 import com.main.volunteer.domain.comment.entity.Comment;
 import com.main.volunteer.domain.group.entity.Group;
+import com.main.volunteer.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findCommentByGroup(Group group);
+
+    Optional<Comment> findByCommentIdAndMember(long commentId, Member member);
 }
