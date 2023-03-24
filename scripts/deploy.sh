4f55a323bc4ff25a9a5c664329509f9f758e7b62
EunSeo Kang <62414231+kdmstj@users.noreply.github.com>
2023-03-24 09:08:34 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/volunteer-0.0.1-SNAPSHOT.jar)
+
+
+BUILD_JAR=$(ls /home/ubuntu/action/server/volunteer/build/libs/volunteer-0.0.1-SNAPSHOT.jar)
 JAR_NAME=$(basename $BUILD_JAR)
+
+sudo chmod 777 /home/ubuntu/action/
 
 echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
 
@@ -22,9 +26,10 @@ else
   sleep 5
 fi
 
+source ~/.bash_profile
 
-source ~/.bashrc
+echo "datasource_url : $datasource_url" >> /home/ubuntu/action/deploy.log
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+sudo nohup java -jar $DEPLOY_JAR & >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
