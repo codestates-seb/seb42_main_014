6b409e478fbc641fab8ce8881a38555db6ff3fc5
jellyjw <104891203+jellyjw@users.noreply.github.com>
2023-04-27 13:36:28 +0900

    Delete client directory

--- client/src/api/axiosInstance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-	baseURL: "http://3.35.252.234:8080",
-	headers: { Authorization: `${localStorage.getItem("accessToken")}` },
-});
-
-export default instance;
--- client/src/utils/theme.ts
+++
@@ -1,3 +0,0 @@
-export const theme = {
-	primary: "#19994C",
-};
